17f75ca2c4b1eab4e33b8ad22b71828d2fd54686
Comfort <65386761+cnorteye@users.noreply.github.com>
2024-09-17 11:36:49 -0700

    Update PrimesTestCase.py with more unit tests

--- v0/PrimesTestCase.py
+++ v0/PrimesTestCase.py
@@ -6,6 +6,16 @@ class PrimesTestCase(unittest.TestCase):
       self.assertTrue(is_prime(5)) 
 	   
    #<Include your test here>
+   def test_is_seven_prime(self):    
+      self.assertTrue(is_prime(7)) 
+
+   def test_is_twentyfive_prime(self):    
+      self.assertTrue(is_prime(25)) 
+
+   def test_is_one_prime(self):
+	   self.assertTrue(is_prime(1))
+
+
 
    
 
